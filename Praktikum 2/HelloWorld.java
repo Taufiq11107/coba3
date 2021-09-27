6ca252dc09c022aea808af91d234884ab4de71fb
Taufiq11107 <71370280+Taufiq11107@users.noreply.github.com>
2021-09-27 16:50:00 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String [] args) {
+        System.out.println("Nama : Taufiq");
+        System.out.println("Nim : 6706202006");
+        System.out.println("Kelas : 45-03");
+    }
+}
